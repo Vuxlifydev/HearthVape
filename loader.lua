163cf06899e51b89d8946864ba205eb22842a8ff
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 18:54:12 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,19 @@
+local modules = {
+    [16522953236] = "PoisonCandy"
+}
 
+local function loadModule(name)
+    local url = "https://raw.githubusercontent.com/vuxliydev/Hearthvape/main/Modules/" .. name .. ".lua"
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(url))()
+    end)
+    if not success then
+        warn("Hearthvape: Failed to load module - " .. tostring(result))
+    end
+end
+
+if modules[game.PlaceId] then
+    loadModule(modules[game.PlaceId])
+else
+    warn("Hearthvape: Unsupported game. No module available.")
+end
