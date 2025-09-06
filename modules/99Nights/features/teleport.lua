4fab757fa99e2d06c8b05f6f124c47b5debf38dc
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 22:35:45 -0500

    Update teleport.lua

--- modules/99Nights/features/teleport.lua
+++ modules/99Nights/features/teleport.lua
@@ -1,1 +1,5 @@
-
+local locations = {
+    Campfire = Vector3.new(100, 5, -200),
+    Trader = Vector3.new(-50, 3, 120)
+}
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(locations["Campfire"])
