b9df0d01666a782ef29bfa7adc9a6440b24fb45e
VuxlifyDev <vuxnpc@gmail.com>
2025-09-05 22:37:23 -0500

    Update auto_collect.lua

--- modules/99Nights/features/auto_collect.lua
+++ modules/99Nights/features/auto_collect.lua
@@ -1,1 +1,12 @@
+local targets = {
+    "Friescrap", "Scrap", "MetalChunk", "Log", "Pelt", "Cloth", "Bone", "Resin",
+    "Meat", "Berry", "Carrot", "CookedMeat", "Water", "Fuel", "Junk",
+    "Totem", "Key", "FairyItem", "TraderItem"
+}
 
+for _, obj in pairs(workspace:GetDescendants()) do
+    if table.find(targets, obj.Name) and obj:IsA("BasePart") then
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, obj, 0)
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, obj, 1)
+    end
+end
